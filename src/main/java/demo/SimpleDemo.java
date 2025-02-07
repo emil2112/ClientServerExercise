f6d5966e8443d81a140d845a755204994662960f
Emil Kriegel <emilkriegel@gmail.com>
2025-02-07 10:05:10 +0100

    First commit
    Jeg giver op gg

--- src/main/java/demo/SimpleDemo.java
+++ src/main/java/demo/SimpleDemo.java
@@ -1,11 +1,13 @@
 package demo;
 
+import javax.print.attribute.standard.DateTimeAtCreation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.time.LocalDateTime;
 
 public class SimpleDemo {
     private ServerSocket server;
@@ -44,16 +46,35 @@ public class SimpleDemo {
         }
         @Override
         public void run(){
-            try {
-                System.out.println("Client registered");
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                String msg = in.readLine();
-                System.out.println("Message from client: " + msg);
+                try {
+                    System.out.println("Client registered \r\n " + clientSocket.getRemoteSocketAddress() +"\r\n Type exit to quit.");
+                    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    while(true) {
+                        String userInput = in.readLine();
+                        if (userInput.equals("exit")) {
+                            break;
+                        }
+                        System.out.println("Message from client: " + userInput);
+                        out.println("Response from server: " + userInput);
+                        if(userInput.equals("exit")){
+                            break;
+                        }
+                        if(userInput.contains("GET")) {
 
-            }catch(IOException e){
-                e.printStackTrace();
-            }
+                            out.println(helloRequest());
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
         }
     }
+    public static String helloRequest(){
+        return "HTTP/1.1 200 OK\r\n"+ "Date: " + LocalDateTime.now() + "\r\n" + "Server: localhost\r\n" +
+                "Content-Type: text/html; charset=UTF-8" + "Content-Length: 12345"+ "\r\n" + "\r\nHello World!";
+    }
+
+    public static String
 }
